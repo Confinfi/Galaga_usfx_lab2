822dd246384a40b350359f1441dbd8d0122904e0
Daniel Aguirre <aguirredanny66@gmail.com>
2024-06-14 10:01:14 -0400

    memento

---
+++ Source/Galaga_usfx_lab2/IOriginator.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IOriginator.h"
+
+// Add default functionality here for any IIOriginator functions that are not pure virtual.
